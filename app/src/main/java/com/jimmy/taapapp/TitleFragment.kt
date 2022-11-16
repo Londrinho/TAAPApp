b0f6574a46a203ecac3303b6d3ebc6bfc3e6af7f
JimmyApara <jimmyapara@gmail.com>
2022-11-16 12:22:20 +0000

    First commit in development of promo app.

---
+++ app/src/main/java/com/jimmy/taapapp/TitleFragment.kt
@@ -0,0 +1,27 @@
+package com.jimmy.taapapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.jimmy.taapapp.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+    private lateinit var binding: FragmentTitleBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        return binding.root
+    }
+
+
+}
